a15a83376d3393cbf29bdd93f33df49009f1b92f
glhrmv <gvmgui@gmail.com>
2017-12-03 12:25:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent2017
+
+My [Advent of Code](https://adventofcode.com/) 2017 attempts.
---
+++ lib/advent2017.ex
@@ -0,0 +1,9 @@
+defmodule Advent2017 do
+  @moduledoc """
+  Documentation for Advent2017.
+  """
+  
+  def hello do
+    :world
+  end
+end

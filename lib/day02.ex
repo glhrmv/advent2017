3a1dce8ba168c5b53dec107bfc752eb8fc73bedd
glhrmv <gvmgui@gmail.com>
2017-12-04 13:52:30 +0000

    Solve day 2 part 2

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,27 +1,50 @@
 defmodule Advent2017.Day02 do
-  defmodule Part1 do
-    @input File.read!("inputs/day02.txt")
-    
-    def run do
-      @input
-      |> parse
-      |> Enum.map(&row_difference/1)
-      |> Enum.sum
-      |> IO.inspect
-    end
-    
-    def parse(input) do
-      input
-      |> String.split("\n", trim: true) # Split line by line
-      |> Enum.map(fn line -> 
-        line 
-        |> String.split # For each line, split on every whitespace
-        |> Enum.map(&String.to_integer/1) # Turn the string values into numbers
-      end)
-    end
-    
-    def row_difference(row) do
-      Enum.max(row) - Enum.min(row) # Difference between the max and min values
-    end
+  @input File.read!("inputs/day02.txt")
+  
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true) # Split line by line
+    |> Enum.map(fn line -> 
+      line 
+      |> String.split # For each line, split on every whitespace
+      |> Enum.map(&String.to_integer/1) # Turn the string values into numbers
+    end)
+  end
+  
+  def row_difference(row) do
+    Enum.max(row) - Enum.min(row) # Difference between the max and min values
+  end
+  
+  def divisible_elements(row) do
+    even_elems = for x <- row, y <- row, x != y, rem(x, y) == 0, do: div(x, y)
+    hd(even_elems)
+  end
+  
+  def input do
+    @input
+  end
+end
+
+defmodule Advent2017.Day02.Part1 do
+  import Advent2017.Day02
+  
+  def run do
+    input()
+    |> parse
+    |> Enum.map(&row_difference/1)
+    |> Enum.sum
+    |> IO.inspect
+  end
+end
+
+defmodule Advent2017.Day02.Part2 do
+  import Advent2017.Day02
+  
+  def run do
+    input()
+    |> parse
+    |> Enum.map(&divisible_elements/1)
+    |> Enum.sum
+    |> IO.inspect
   end
 end

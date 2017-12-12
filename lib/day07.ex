a77b52770a1820fda1a4c0f4d00e04d4eae52811
glhrmv <gvmgui@gmail.com>
2017-12-12 14:29:50 +0000

    Add day 7 boilerplate

---
+++ lib/day07.ex
@@ -0,0 +1,16 @@
+defmodule Advent2017.Day07 do
+  @input File.read!("inputs/day07.txt")
+  
+  def input do
+    @input
+  end
+end
+
+defmodule Advent2017.Day07.Part1 do
+  import Advent2017.Day07
+  
+  def run do
+    input()
+    |> IO.inspect
+  end
+end

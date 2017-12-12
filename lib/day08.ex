e3609d37505441d71159ca8befd8b0ba024cbabd
glhrmv <gvmgui@gmail.com>
2017-12-12 14:57:34 +0000

    Finish day 8 part 1

--- lib/day08.ex
+++ lib/day08.ex
@@ -6,7 +6,54 @@ defmodule Advent2017.Day08 do
   end
   
   def parse_instructions(input) do
-    [reg, op, arg, "if", ctarget, cop, carg] = String.split(input)
+    [reg, op, arg, "if", ctarget, copt, carg] = String.split(input)
+    
+    %{
+      reg: reg,
+      op: op,
+      arg: String.to_integer(arg),
+      ctarget: ctarget,
+      copt: copt,
+      carg: String.to_integer(carg)
+    }
+  end
+  
+  def check_condition(curr, registers) do
+    case curr.copt do
+      ">" -> Map.get(registers, curr.ctarget, 0) > curr.carg
+      "<" -> Map.get(registers, curr.ctarget, 0) < curr.carg
+      ">=" -> Map.get(registers, curr.ctarget, 0) >= curr.carg
+      "==" -> Map.get(registers, curr.ctarget, 0) == curr.carg
+      "<=" -> Map.get(registers, curr.ctarget, 0) <= curr.carg
+      "!=" -> Map.get(registers, curr.ctarget, 0) != curr.carg
+    end
+  end
+
+  def update_with(curr, registers, fun) do
+    updated = fun.(Map.get(registers, curr.reg, 0), curr.arg)
+    
+    Map.put(registers, curr.reg, updated)
+  end
+
+  def update_registers(curr, registers) do
+    case curr.op do
+      "inc" -> update_with(curr, registers, &(&1 + &2))
+      "dec" -> update_with(curr, registers, &(&1 - &2))
+    end
+  end
+
+  def run(input, registers \\ %{})
+  def run([curr | rest], reg) do
+    if check_condition(curr, reg) do
+      reg = update_registers(curr, reg)
+      run(rest, reg)
+    else
+      run(rest, reg)
+    end
+  end
+  
+  def run([], reg) do
+    reg
   end
 end
 
@@ -17,6 +64,9 @@ defmodule Advent2017.Day08.Part1 do
     input()
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_instructions/1)
+    |> run
+    |> Map.values
+    |> Enum.max
     |> IO.inspect
   end
 end

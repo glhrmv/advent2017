4952bebc33be15733e553b0b961aecd2c76773b0
glhrmv <gvmgui@gmail.com>
2017-12-14 21:01:11 +0000

    Solve day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -5,14 +5,21 @@ defmodule Advent2017.Day04 do
     @input
   end
   
-  def validate(list_of_passphrases) do
-    list_of_passphrases
-    |> Enum.map(&String.split/1) # Split each passphrase into a list of words
-    |> Enum.filter(fn p ->
+  def validate(list) do
+    list
+    |> Enum.filter(fn passphrase ->
       # Is the passphrase's length the same as the length of 
       # all of its unique elements? If so, it is valid (no duplicates)
-      length(p) == length(Enum.uniq(p))
+      passphrase == Enum.uniq(passphrase)
     end)
+  end
+  
+  def check_for_anagrams(passphrase) do
+    # To check for anagrams, sort every passphrase's word
+    # so that all possible anagrams are duplicates
+    passphrase
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> Enum.map(&Enum.sort/1)
   end
 end
 
@@ -22,6 +29,21 @@ defmodule Advent2017.Day04.Part1 do
   def run do
     input()
     |> String.split("\n", trim: true)
+    |> Enum.map(&String.split/1)
+    |> validate
+    |> Enum.count
+    |> IO.inspect
+  end
+end
+
+defmodule Advent2017.Day04.Part2 do
+  import Advent2017.Day04
+  
+  def run do
+    input()
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(&check_for_anagrams/1)
     |> validate
     |> Enum.count
     |> IO.inspect

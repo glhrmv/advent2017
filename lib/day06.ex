76516d8587ac894a4ed2bf294575b29059b150e7
glhrmv <gvmgui@gmail.com>
2017-12-17 22:16:51 +0000

    Clean up day 6 boilerplate

--- lib/day06.ex
+++ lib/day06.ex
@@ -1,9 +1,7 @@
 defmodule Advent2017.Day06 do
-  @input File.read!("inputs/day06.txt") |> String.split("\t")
+  @input File.read!("inputs/day06.txt")
   
-  def input do
-    @input
-  end
+  def input, do: @input
 end
 
 defmodule Advent2017.Day06.Part1 do
@@ -11,6 +9,7 @@ defmodule Advent2017.Day06.Part1 do
   
   def run do
     input()
+    |> String.split("\t")
     |> IO.inspect
   end
 end

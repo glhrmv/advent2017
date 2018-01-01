fd86b0daa9174e32216bb1a4c5eb0d548f0cb45a
glhrmv <gvmgui@gmail.com>
2018-01-01 21:13:53 +0000

    Solve day 13 part 1

--- lib/day13.ex
+++ lib/day13.ex
@@ -2,6 +2,21 @@ defmodule Advent2017.Day13 do
   @input File.read!("inputs/day13.txt")
   
   def input, do: @input
+  
+  def caught?({depth, range}, delay) do
+    rem(depth + delay, 2 * (range - 1)) == 0
+  end
+  
+  def calculate_severity(list, acc \\ 0)
+  def calculate_severity([{depth, range} | rest], acc) do
+    severity = acc + depth * range
+    
+    calculate_severity(rest, severity)
+  end
+  
+  def calculate_severity([], acc) do
+    acc
+  end
 end
 
 defmodule Advent2017.Day13.Part1 do
@@ -9,6 +24,15 @@ defmodule Advent2017.Day13.Part1 do
   
   def run do
     input()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line 
+      |> String.split(": ")
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple
+    end)
+    |> Enum.filter(&caught?(&1, 0))
+    |> calculate_severity
     |> IO.inspect
   end
 end
